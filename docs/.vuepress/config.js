17194965ddcf0ddea053ca379cb52d0c8b4cb5f0
DolaTRPG <dolatrpg@gmail.com>
2019-07-05 18:08:34 +0800

    新增故事：新手們的冒險

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -28,6 +28,18 @@ module.exports = {
                     ['/02/pc-midou.md', '玩家角色：御堂赤彥'],
                     '/02/story.md'
                 ]
+            },
+            {
+                title: '故事3: 新手們的冒險',
+                collapsable: true,
+                children: [
+                    '/03/introduction.md',
+                    ['/03/pc-alex.md', '玩家角色：艾力克斯'],
+                    ['/03/pc-nanachi.md', '玩家角色：娜娜奇'],
+                    ['/03/pc-haku.md', '玩家角色：哈克．扣卡'],
+                    '/03/prepare.md',
+                    '/03/story.md'
+                ]
             }
         ]
     }
---
+++ docs/03/introduction.md
@@ -0,0 +1,25 @@
+# 團務簡介
+
+```
+邊境小鎮的冒險者公會裡面聚集了一群新手冒險者
+公會分配給這群新手們的第一個任務是去鏟除附近的哥布林巢穴
+
+雖然只是怪物之中最弱小的哥不林
+但是公會的櫃台小姐仍然非常謹慎地警告冒險者千萬不要大意
+甚至引用了冒險者前輩們的忠告：
+  "冒險者不該冒險"
+  "做好萬全的準備，不要讓神明擲骰子"
+```
+
+## 參與人員
+
+- 主持人：Dola
+- 玩家
+  - 百鬼夜行 as 艾力克斯
+  - 枯馬是kuma as 娜娜奇
+  - 藍天豬腳 as 萊森
+  - Nelson as 哈克
+
+## 實況影片
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/8h-hx5lw1nk" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
